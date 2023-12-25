9972bd46cc8796bd025433cd1185fd4fb7707716
A.Protasevich <larsplotner@gmail.com>
2023-12-25 22:38:27 +0300

    feat: fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import * as path from "path"
+import { readFile } from "fs/promises";
+
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        const filePathToRead = path.resolve("./src/fs/files/fileToRead.txt")
+        const fileContent = await readFile(filePathToRead, "utf-8")
+        console.log(fileContent)
+    } catch (err) {
+        throw new Error("FS operation failed")
+    }
 };
 
 await read();
