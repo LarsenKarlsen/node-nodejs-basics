900bcccaea7a3130e9b1c8f08470cfecaf100013
A.Protasevich <larsplotner@gmail.com>
2023-12-25 21:52:22 +0300

    feat: fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import {writeFile} from "node:fs/promises"
+import * as path from "node:path"
+
+
 const create = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        const filePath = path.normalize("./src/fs/files/fresh.txt")
+        await writeFile(filePath, "I am fresh and young", {flag:"wx"})
+    }
+    catch(err) {
+        throw new Error("FS operation failed")
+    }
 };
 
 await create();
