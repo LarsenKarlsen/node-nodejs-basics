350fdc002be3162b9af3711361a69effde2ac237
A.Protasevich <larsplotner@gmail.com>
2023-12-25 22:24:11 +0300

    feat FS:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import * as path from "path"
+import { readdir } from "fs/promises";
+
+
 const list = async () => {
-    // Write your code here 
+    // Write your code here
+    try{
+        const curDir = path.resolve(".", "src", "fs")
+        const listDir = path.join(curDir, "files")
+    
+        const fileNames = await readdir(listDir)
+        console.log(fileNames)
+    } catch(err) {
+        throw new Error("FS operation failed")
+    }
+
 };
 
 await list();
