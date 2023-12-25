bc0b1f07cc36c6601b7b61c2ed8eb7332599bcf1
A.Protasevich <larsplotner@gmail.com>
2023-12-25 21:58:22 +0300

    feat: fs:delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import * as path from "node:path"
+import { unlink } from "node:fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        const filePath = path.normalize("./src/fs/files/fileToRemove.txt")
+        await unlink(filePath)
+    } catch (err) {
+        throw new Error("FS operation failed")
+    }
+
 };
 
 await remove();
