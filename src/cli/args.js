770083065c58f3d052da0c2491a8875c400afa41
A.Protasevich <larsplotner@gmail.com>
2023-12-26 20:25:21 +0300

    feat: cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+    const argsList = process.argv.slice(2)
+    let resp = []
+    for (let i = 0; i<argsList.length-1; i+=2) {
+        resp.push(`${argsList[i].slice(2)} is ${argsList[i+1]}`)
+    }
+    console.log(resp.join(", "))
 };
 
 parseArgs();
