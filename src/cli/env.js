653250d7212af8e2ee8607527585691ae58444b1
A.Protasevich <larsplotner@gmail.com>
2023-12-26 20:18:05 +0300

    feat: cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    const processEnv = process.env
+    let res = ""
+    for (const key in processEnv) {
+        if (key.includes("RSS_")) {
+            res += `${key}=${processEnv[key]}; `
+        }
+    }
+
+    console.log(res)
 };
 
 parseEnv();
