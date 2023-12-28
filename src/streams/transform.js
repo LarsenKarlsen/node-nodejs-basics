f1a2dec2fe0ba950324ee0461543919d1f44e80b
A.Protasevich <larsplotner@gmail.com>
2023-12-28 15:01:31 +0300

    feat: streams:transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import {Transform} from "stream"
+
 const transform = async () => {
-    // Write your code here 
+    // Write your code here
+    const reverse = new Transform({
+        transform(data, encoding, callback) {
+            const reversedString = data.toString().split("").reverse().join("")
+            this.push(reversedString)
+            callback()
+        }
+    })
+
+    process.stdin.pipe(reverse).on("data", data => {
+        process.stdout.write(`Reverse string => ${data.toString().trim()}`)
+        process.exit()
+    })
 };
 
 await transform();
