cfadff8f421732d2eda677fb69f4b0ea08e088af
A.Protasevich <larsplotner@gmail.com>
2023-12-28 14:34:31 +0300

    feat: streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import fs from "fs"
+import * as path from "path"
+
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = path.resolve("./src/streams/files/fileToRead.txt")
+    const rs = fs.createReadStream(filePath, "utf-8")
+
+    rs.on("error", err => {
+        console.log(err)
+    })
+
+    rs.on("data", data => {
+        process.stdout.write(data)
+    })
 };
 
 await read();
