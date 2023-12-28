e9fda75ec8d2cf0c60170d27bf475d5511f97804
A.Protasevich <larsplotner@gmail.com>
2023-12-28 12:24:20 +0300

    feat: calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import * as path from "path"
+import { readFile } from "fs/promises"
+import crypto from "crypto"
+import {createReadStream} from 'node:fs';
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = path.resolve("src/hash/files/fileToCalculateHashFor.txt")
+    const {createHmac} = await import('node:crypto');
+    const hmac = createHmac('sha256', 'topSecretWord');
+    const input = createReadStream(filePath);
+    input.on('readable', () => {
+        const data = input.read();
+        if (data) {
+            hmac.update(data);
+        } else {
+            console.log(`${hmac.digest('hex')} ${filePath}`);
+        }
+    });
 };
 
 await calculateHash();
